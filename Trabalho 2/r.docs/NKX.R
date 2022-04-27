6080287d511d1b602799f1c31593ad7e13363e93
penasta <toledis@gmail.com>
2022-04-27 16:17:24 -0300

    nKx

---
+++ Trabalho 2/r.docs/NKX.R
@@ -0,0 +1,28 @@
+library(lubridate)
+DOPA$DTOBITO2 <- NA
+DOPA$DTOBITO2 <- dmy(DOPA$DTOBITO)
+
+DOPE$DTOBITO2 <- NA
+DOPE$DTOBITO2 <- dmy(DOPE$DTOBITO)
+
+DOPA$DIF <- NA
+DOPE$DIF <- NA
+
+um <- ("2019-01-01")
+
+DOPA$DIF <- difftime(DOPA$DTOBITO2,um, units="days")/365.25
+DOPA$DIF<-as.numeric(DOPA$DIF)
+is.numeric(DOPA$DIF)
+
+DOPE$DIF <- difftime(DOPE$DTOBITO2,um, units="days")/365.25
+DOPE$DIF<-as.numeric(DOPE$DIF)
+is.numeric(DOPE$DIF)
+
+# Falta arrumar os valores maiores que 1 na variável DOPE$DIF
+# Após isso, somar e tirar a média de DIF por IDADE SIMPLES
+# Após isso, agregar pelos grupos quinquenais de idade. Está pronto o nKx.
+
+#----------------------------------------------------------
+
+write.csv(DOPA,"DOPA2.csv")
+write.csv(DOPE,"DOPE2.csv")
